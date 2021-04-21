9335434225498900a747603ff48193ac91c259ca
Andrew Zhu <me@andrew-zhu.com>
2021-04-20 20:03:02 -0700

    update db connection

--- paissadb/config.py
+++ paissadb/config.py
@@ -1,6 +1,8 @@
 import os
 
 GAMEDATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../gamedata")
-JWT_SECRET_PAISSAHOUSE = os.getenv("JWT_SECRET_PAISSAHOUSE")
 JWT_ISSUER = 'PaissaDB'
 JWT_AUDIENCES = ['PaissaHouse']
+
+JWT_SECRET_PAISSAHOUSE = os.getenv("JWT_SECRET_PAISSAHOUSE")
+DB_URI = os.getenv("DB_URI", "sqlite:///./sql_app.db")
--- paissadb/database.py
+++ paissadb/database.py
@@ -1,12 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+from . import config
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine_kwargs = {}
+
+if config.DB_URI.startswith("sqlite://"):
+    engine_kwargs.update(connect_args={"check_same_thread": False})
+
+engine = create_engine(config.DB_URI, **engine_kwargs)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
