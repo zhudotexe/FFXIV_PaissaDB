9144ebe27034d22f25d9673a5b51c9d6b22bb6ec
Andrew Zhu <me@andrew-zhu.com>
2021-04-20 23:37:38 -0700

    better db type parsing

--- paissadb/config.py
+++ paissadb/config.py
@@ -1,4 +1,5 @@
 import os
+import urllib.parse
 
 GAMEDATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../gamedata")
 JWT_ISSUER = 'PaissaDB'
@@ -6,4 +7,4 @@ JWT_AUDIENCES = ['PaissaHouse']
 
 JWT_SECRET_PAISSAHOUSE = os.getenv("JWT_SECRET_PAISSAHOUSE")
 DB_URI = os.getenv("DB_URI", "sqlite:///./sql_app.db")
-DB_TYPE = DB_URI.split('://')[0]
+DB_TYPE = urllib.parse.urlparse(DB_URI).scheme.split('+')[0]
