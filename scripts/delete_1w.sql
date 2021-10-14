df6d65ad927432a151bd0e48ea46a14e63668d15
Andrew Zhu <me@andrew-zhu.com>
2021-10-14 12:23:02 -0400

    chore: make vacuum verbose and specific

--- scripts/delete_1w.sql
+++ scripts/delete_1w.sql
@@ -1,6 +1,7 @@
--- Deletes events older than 3 days from when this script is run, and all child rows (due to cascading).
+-- Deletes events older than 2 days from when this script is run, and all child rows (due to cascading).
 DELETE
 FROM paissadb.public.events
-WHERE paissadb.public.events.timestamp < now() - '3 day'::interval;
+WHERE paissadb.public.events.timestamp < NOW() - '2 day'::interval;
 
-VACUUM FULL;  -- only run this in the dead of night, because it locks the db!
+-- only run this in the dead of night, because it locks the db!
+VACUUM (FULL, VERBOSE) paissadb.public.events, paissadb.public.plots, paissadb.public.wardsweeps;
