cadfaf0f817df6bf9dff8e442b77093fce542fa0
Andrew Zhu <me@andrew-zhu.com>
2022-07-05 00:31:39 -0700

    fix: db cleanup script datatype

--- scripts/delete_1w.sql
+++ scripts/delete_1w.sql
@@ -1,11 +1,11 @@
 -- Deletes events and plot states older than 9 days from when this script is run.
 DELETE
 FROM paissadb.public.events
-WHERE paissadb.public.events.timestamp < NOW() - '9 day'::interval;
+WHERE paissadb.public.events.timestamp < EXTRACT(EPOCH FROM NOW() - '9 day'::interval);
 
 DELETE
 FROM plot_states
-WHERE lotto_phase_until < NOW() - '9 day'::interval;
+WHERE lotto_phase_until < EXTRACT(EPOCH FROM NOW() - '9 day'::interval);
 
 -- only run this in the dead of night, because it locks the db!
 VACUUM (FULL, VERBOSE) paissadb.public.events;
