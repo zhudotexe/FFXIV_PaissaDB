e7ea22f7b5e9c0c548c0d92e2728affa53a4cf33
Andrew Zhu <me@andrew-zhu.com>
2022-07-05 00:25:23 -0700

    fix: backup filename timestamp

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Dump the DB to stdout with maximum compression, and stream that to S3
 
+timestamp=$(date +%s)
 sudo -u paissadb pg_dump --no-owner -v -Z 9 paissadb | aws s3 cp - s3://paissadb-historical/paissadb-${timestamp}.sql.gz
