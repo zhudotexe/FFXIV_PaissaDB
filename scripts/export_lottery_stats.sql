aaca1afb58dbfa7be40c90d1bdf7d216000fc9ba
Andrew Zhu <me@andrew-zhu.com>
2023-06-07 20:54:29 -0700

    misc

--- scripts/export_lottery_stats.sql
+++ scripts/export_lottery_stats.sql
@@ -1,9 +1,13 @@
 -- export_lottery_stats
 -- Exports the entry stats from the most recent entry cycle, ordered by entry count descending.
--- FIXME this does not account for plots not seen from placard
 -- lottery_export
-WITH constants (lotto_cycle, cycle_mod)
-         AS (VALUES (777600, 54000))
+WITH constants (lotto_cycle, cycle_mod, entry_time)
+         AS (VALUES (777600, 54000, 432000)),
+     times (end_time)
+         AS (SELECT FLOOR((EXTRACT(EPOCH FROM NOW()) - constants.cycle_mod) / constants.lotto_cycle) *
+                    constants.lotto_cycle +
+                    constants.cycle_mod
+             FROM constants)
 -- ^ set to seconds in 9 days, offset within 9-day cycle (e.g. 1658674800 % 777600 = 54000)
 SELECT w.name                      AS world,
        d.name                      AS district,
@@ -20,7 +24,8 @@ FROM plot_states s
          LEFT JOIN districts d ON d.id = s.territory_type_id
          LEFT JOIN worlds w ON w.id = s.world_id
          JOIN constants ON TRUE
-WHERE lotto_phase_until =
-      FLOOR((EXTRACT(EPOCH FROM NOW()) - constants.cycle_mod) / constants.lotto_cycle) * constants.lotto_cycle +
-      constants.cycle_mod
+         JOIN times ON TRUE
+WHERE (lotto_phase_until = times.end_time
+    OR (last_seen >= times.end_time - constants.entry_time AND first_seen < times.end_time))
+  AND is_owned = FALSE
 ORDER BY lotto_entries DESC NULLS LAST;
